bbb49331f13b9ee635ee64ee6dfae4cc4ee160f3
Felix Lindgren <felli675@student.liu.se>
2020-04-21 20:56:09 +0200

    Adds filtering social-id and name, dragging columns and created most columns for the finished table

--- client/src/app/overview-table/overview-table.component.ts
+++ client/src/app/overview-table/overview-table.component.ts
@@ -5,34 +5,92 @@ import { ColumnMode } from '@swimlane/ngx-datatable/public-api';
 @Component({
   selector: 'app-overview-table',
   templateUrl: './overview-table.component.html',
-  styleUrls: ['./overview-table.component.css']
+  styleUrls: ['./overview-table.component.scss']
   
 })
 export class OverviewTableComponent implements OnInit {
   @ViewChild('myTable') table:any; 
   
-  
-  
-  
-  rows = [
-    { team:'A', name: 'Austin', gender: 'Male', company: 'Swimlane', age:22, respons:"DR. XYZ, SSK ABC, USK. JKL" },
-    { team:'B', name: 'Dany', gender: 'Male', company: 'KFC', age:33, respons:"DR. XYZ, SSK ABC, USK. JKL" },
-    { team:'B', name: 'Bany', gender: 'Male', company: 'KFC', age:33, respons:"DR. XYZ, SSK ABC, USK. JKL" },
-    { team:'B', name: 'Chany', gender: 'Male', company: 'KFC', age:33, respons:"DR. XYZ, SSK ABC, USK. JKL" },
-    { team:'C', name: 'Molly', gender: 'Female', company: 'Burger King' , age:22, respons:"DR. XYZ, SSK ABC, USK. JKL"} ,
-    { team:'C', name: 'Jenny', gender: 'Female', company: 'Burger King' , age:22, respons:"DR. XYZ, SSK ABC, USK. JKL"} 
-  ];
-  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
 
+  getCellClass({ row, column, value }): any {
+    return {
+      'prio-blue': value === 'blue',
+      'prio-green': value === 'green',
+      'prio-yellow': value === 'yellow',
+      'prio-orange': value === 'orange',
+      'prio-red': value === 'red'
+
+    };
+  }
+  
+  getRowClass(row):any{
+    return {'row' : true};
+    
+  }
+  
   funder = [];
   calculated = [];
   pending = [];
   groups = [];
-  
   editing = {};
   
+  temp = [
+    {prio:"blue", social: "980808-7890", team:'A', name:'Austin',  gender: 'Male', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"   },
+    {prio:"yellow", social: "010101-7890", team:'B', name: 'Dany', gender: 'Male', age:33, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"   },
+    {prio:"orange", social: "123456-7890", team:'B', name: 'Bany', gender: 'Male', age:33, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"   },
+    {prio:"blue", social: "123456-7890", team:'B', name: 'Chany', gender: 'Male', age:33, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"   },
+    {prio:"green", social: "123456-7890", team:'C', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"red", social: "123456-7890", team:'C', name: 'Jenny', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'D', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'D', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'D', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'U', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'U', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'U', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+    {prio:"green", social: "123456-7890", team:'X', name: 'Molly', gender: 'Female', age:22, dr:"Rakeeb", nurse: "Anna", nurse2:"erik", arrival:"00:00", search:"buksm 178", activity:"button", time: "10 min", arrival_method:"ambulance"  },
+
+  ];
+  rows = this.temp;
+  
+
   
   
+  updateFilter(event) {
+    var val = event.target.value;
+    const num = isNaN(val);
+    console.log(num);
+    // filter our data
+    var temp = [];
+    if (num) {
+      val = val.toLowerCase();
+      temp = this.temp.filter(function (d) {
+        return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+      });    
+    } else {
+      temp = this.temp.filter(function (d) {
+        return d.social.toLowerCase().indexOf(val) !== -1 || !val;
+      });  
+    }
+    this.rows = temp;
+    
+  }
+
+  doubleclick(abc:any){
+    console.log("doubleclick");
+  }
+  
+  activityClicked(event){
+    console.log("Activity clicked");
+  }
 
   constructor() { }
 
@@ -50,109 +108,6 @@ export class OverviewTableComponent implements OnInit {
     return style;
   }
 
-  checkGroup(event, row, rowIndex, group) {
-    let groupStatus = 'Pending';
-    let expectedPaymentDealtWith = true;
-
-    row.exppayyes = 0;
-    row.exppayno = 0;
-    row.exppaypending = 0;
-
-    if (event.target.checked) {
-      if (event.target.value === '0') {
-        // expected payment yes selected
-        row.exppayyes = 1;
-      } else if (event.target.value === '1') {
-        // expected payment yes selected
-        row.exppayno = 1;
-      } else if (event.target.value === '2') {
-        // expected payment yes selected
-        row.exppaypending = 1;
-      }
-    }
-
-    if (group.length === 2) {
-      // There are only 2 lines in a group
-      // tslint:disable-next-line:max-line-length
-      if (
-        ['Calculated', 'Funder'].indexOf(group[0].source) > -1 &&
-        ['Calculated', 'Funder'].indexOf(group[1].source) > -1
-      ) {
-        // Sources are funder and calculated
-        // tslint:disable-next-line:max-line-length
-        if (group[0].startdate === group[1].startdate && group[0].enddate === group[1].enddate) {
-          // Start dates and end dates match
-          for (let index = 0; index < group.length; index++) {
-            if (group[index].source !== row.source) {
-              if (event.target.value === '0') {
-                // expected payment yes selected
-                group[index].exppayyes = 0;
-                group[index].exppaypending = 0;
-                group[index].exppayno = 1;
-              }
-            }
-
-            if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0) {
-              expectedPaymentDealtWith = false;
-            }
-            console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
-          }
-        }
-      }
-    } else {
-      for (let index = 0; index < group.length; index++) {
-        if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0) {
-          expectedPaymentDealtWith = false;
-        }
-        console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
-      }
-    }
-
-    // check if there is a pending selected payment or a row that does not have any expected payment selected
-    if (
-      group.filter(rowFilter => rowFilter.exppaypending === 1).length === 0 &&
-      group.filter(rowFilter => rowFilter.exppaypending === 0 && rowFilter.exppayyes === 0 && rowFilter.exppayno === 0)
-        .length === 0
-    ) {
-      console.log('expected payment dealt with');
-
-      // check if can set the group status
-      const numberOfExpPayYes = group.filter(rowFilter => rowFilter.exppayyes === 1).length;
-      const numberOfSourceFunder = group.filter(rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Funder')
-        .length;
-      const numberOfSourceCalculated = group.filter(
-        rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Calculated'
-      ).length;
-      const numberOfSourceManual = group.filter(rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Manual')
-        .length;
-
-      console.log('numberOfExpPayYes', numberOfExpPayYes);
-      console.log('numberOfSourceFunder', numberOfSourceFunder);
-      console.log('numberOfSourceCalculated', numberOfSourceCalculated);
-      console.log('numberOfSourceManual', numberOfSourceManual);
-
-      if (numberOfExpPayYes > 0) {
-        if (numberOfExpPayYes === numberOfSourceFunder) {
-          groupStatus = 'Funder Selected';
-        } else if (numberOfExpPayYes === numberOfSourceCalculated) {
-          groupStatus = 'Calculated Selected';
-        } else if (numberOfExpPayYes === numberOfSourceManual) {
-          groupStatus = 'Manual Selected';
-        } else {
-          groupStatus = 'Hybrid Selected';
-        }
-      }
-    }
-
-    group[0].groupstatus = groupStatus;
-  }
-
-  updateValue(event, cell, rowIndex) {
-    this.editing[rowIndex + '-' + cell] = false;
-    this.rows[rowIndex][cell] = event.target.value;
-    this.rows = [...this.rows];
-  }
-
   toggleExpandGroup(group) {
     console.log('Toggled Expand Group!', group);
     this.table.groupHeader.toggleExpandGroup(group);
@@ -161,5 +116,4 @@ export class OverviewTableComponent implements OnInit {
   onDetailToggle(event) {
     console.log('Detail Toggled', event);
   }
-
 }
