34cfba1180ff50b9f4918bfadef568fd60c02d0d
simon <simon@simonws.se>
2020-04-28 11:56:21 +0200

    Add tests for custom datePipe

--- client/src/app/date-pipe.pipe.spec.ts
+++ client/src/app/date-pipe.pipe.spec.ts
@@ -1,8 +1,26 @@
 import { DatePipePipe } from './date-pipe.pipe';
 
 describe('DatePipePipe', () => {
+  const dayMs = 1000 * 3600 * 24;
+  const dayPipe = new DatePipePipe();
+
   it('create an instance', () => {
     const pipe = new DatePipePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should say yesterday', () => {
+    const yesterday = new Date(Date.now() - dayMs);
+    expect(dayPipe.transform(yesterday) === 'Igår');
+  });
+
+  it('should say tomorrow', () => {
+    const tomorrow = new Date(Date.now() + dayMs);
+    expect(dayPipe.transform(tomorrow) === 'Imorgon');
+  });
+
+  it('should say today', () => {
+    const today = new Date(Date.now());
+    expect(dayPipe.transform(today) === 'Idag');
   });
 });
