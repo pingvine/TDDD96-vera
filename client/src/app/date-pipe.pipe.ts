fae9167784126f49a39369cfbe84b2aaa8855340
simon <simon@simonws.se>
2020-04-28 09:34:24 +0200

    generate custom date pipe

---
+++ client/src/app/date-pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'datePipe'
+})
+export class DatePipePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
