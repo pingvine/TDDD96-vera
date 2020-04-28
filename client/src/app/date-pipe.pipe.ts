04ba24a5381aa7b3ed19c2eccb26663c15be1cb7
simon <simon@simonws.se>
2020-04-28 10:25:04 +0200

    Add custom date filter for yesterday, today, tomorrow

--- client/src/app/date-pipe.pipe.ts
+++ client/src/app/date-pipe.pipe.ts
@@ -1,12 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Pipe({
   name: 'datePipe'
 })
-export class DatePipePipe implements PipeTransform {
+export class DatePipePipe extends DatePipe implements PipeTransform {
+  transform(date: Date, args?: any): string {
+    const now = new Date(Date.now());
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+    const copyDate = new Date(date.getTime());
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    date.setSeconds(0);
+    date.setHours(0);
+    date.setMinutes(0);
+
+    const diffDays = now.getDay() - date.getDay();
+    let format = '';
+    if (diffDays === 0) {
+      format = 'Idag';
+    } else if (diffDays === 1) {
+      format = 'Igår';
+    } else if (diffDays === -1) {
+      format = 'Imorgon';
+    } else {
+      return super.transform(copyDate, format="hh:mm");
+    }
+
+    return format + ' ' + super.transform(copyDate, format="hh:mm");
   }
-
 }
