b772e881df64a8776aa415e26a7fd8fb95de96ad
simon <simon@simonws.se>
2020-04-29 08:21:59 +0200

    Add components under patient: header and visit reason

---
+++ client/src/app/patient/patient-view-header/patient-view-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-view-header',
+  templateUrl: './patient-view-header.component.html',
+  styleUrls: ['./patient-view-header.component.css']
+})
+export class PatientViewHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/patient/visit-reason/visit-reason.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-reason',
+  templateUrl: './visit-reason.component.html',
+  styleUrls: ['./visit-reason.component.css']
+})
+export class VisitReasonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
