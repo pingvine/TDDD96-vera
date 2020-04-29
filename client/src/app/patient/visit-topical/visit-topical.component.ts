2b41b6ab2ef17f1a5861f28f1d762077e4f15166
simon <simon@simonws.se>
2020-04-29 09:16:53 +0200

    Add topical visit info component

---
+++ client/src/app/patient/visit-topical/visit-topical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-topical',
+  templateUrl: './visit-topical.component.html',
+  styleUrls: ['./visit-topical.component.css']
+})
+export class VisitTopicalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
