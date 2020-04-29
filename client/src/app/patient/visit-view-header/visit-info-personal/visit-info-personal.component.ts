fdf44fb81c9dcad14d0e049a0dbe061435e4ef7a
simon <simon@simonws.se>
2020-04-29 10:29:07 +0200

    Add personal info input and output

--- client/src/app/patient/visit-view-header/visit-info-personal/visit-info-personal.component.ts
+++ client/src/app/patient/visit-view-header/visit-info-personal/visit-info-personal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-visit-info-personal',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visit-info-personal.component.css']
 })
 export class VisitInfoPersonalComponent implements OnInit {
+  // TODO use user model
+  @Input() priority = 0;
+  @Input() firstName = 'firstName';
+  @Input() lastName = 'lastName';
+  @Input() socialId = 199801010101;
+  // @Input() attention: AttentionData;
+
+  @Output() prioChange: EventEmitter<number> = new EventEmitter();
 
   constructor() { }
 
