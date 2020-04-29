292665746c4e7db5d1bbbca37e96d4c93279ed29
simon <simon@simonws.se>
2020-04-29 15:01:37 +0200

    change comment

--- client/src/app/models/Priority.ts
+++ client/src/app/models/Priority.ts
@@ -1,8 +1,8 @@
 export enum Priority {
   '#757575',  // gray, 0
   '#5075FA',  // blue, 1
-  '#3DFF44',  // green, 3
-  '#FBFF3F',  // yellow, 4
-  '#FF9900',  // orange, 5
-  '#FF0000'  // red, 6
+  '#3DFF44',  // green, 2
+  '#FBFF3F',  // yellow, 3
+  '#FF9900',  // orange, 4
+  '#FF0000'  // red, 5
 }
