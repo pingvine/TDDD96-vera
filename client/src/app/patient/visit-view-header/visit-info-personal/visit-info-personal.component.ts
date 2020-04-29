5c0d99217f6f896eab632984fc7313b9f7381fd0
simon <simon@simonws.se>
2020-04-29 09:58:39 +0200

    Add personal info component

---
+++ client/src/app/patient/visit-view-header/visit-info-personal/visit-info-personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-info-personal',
+  templateUrl: './visit-info-personal.component.html',
+  styleUrls: ['./visit-info-personal.component.css']
+})
+export class VisitInfoPersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
