91331ddcc39ae521f66bda9c802d669fdf981a95
simon <simon@simonws.se>
2020-04-29 14:58:40 +0200

    Add priority enum

---
+++ client/src/app/models/Priority.ts
@@ -0,0 +1,8 @@
+export enum Priority {
+  '#757575',  // gray, 0
+  '#5075FA',  // blue, 1
+  '#3DFF44',  // green, 3
+  '#FBFF3F',  // yellow, 4
+  '#FF9900',  // orange, 5
+  '#FF0000'  // red, 6
+}
