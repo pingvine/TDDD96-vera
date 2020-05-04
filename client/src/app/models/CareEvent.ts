bb4e6bcad3ce88ecbe9a5d23fa23d0dc41fb87e0
Jesper Jonsson <jesjo430@student.liu.se>
2020-05-04 15:12:24 +0200

    Add careEvent draft class

--- client/src/app/models/CareEvent.ts
+++ client/src/app/models/CareEvent.ts
@@ -1,1 +1,56 @@
-export class CareEvent{}
+import { ActionType } from './ActionType'
+import { Person } from './Person'
+
+export class CareEvent{
+    private touched: Date
+    private creatorId: number
+    private receiversId: number
+    private completed: [Date, number]
+    private actionType: ActionType
+    private creationTime: Date
+    private comment: string
+
+    constructor(creator: Person, reciever: Person, action: ActionType, comment: string){
+        this.touched = new Date
+        this.creatorId = reciever.getId()
+        this.receiversId = creator.getId()
+        this.completed = undefined
+        this.actionType = action
+        this.creationTime = new Date()
+        this.comment = comment
+    }
+
+    touch(){
+        this.touched = new Date()
+    }
+
+    getCreatorId(){
+        return this.creatorId
+    }
+
+    getReceiverId(){
+        return this.receiversId
+    }
+
+    markAsCompleted(markedBy: number){
+        this.completed = [new Date(), markedBy]
+    }
+
+    getActionType(){
+        return this.actionType
+    }
+
+    getCreationTime(){
+        return this.creationTime
+    }
+
+    getComment(){
+        return this.comment
+    }
+
+    appendComment(comment: string){
+        let lineBreak = '\n'
+        this.comment = this.comment.concat(lineBreak)
+        this.comment = this.comment.concat(comment.toString())
+    }
+}
