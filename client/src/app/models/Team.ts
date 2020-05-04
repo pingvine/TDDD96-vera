8c727e3c2e069f7f5b3a3f738768b8ee1b7fdeb2
Jesper Jonsson <jesjo430@student.liu.se>
2020-05-04 09:51:26 +0200

    Add visits to team model

--- client/src/app/models/Team.ts
+++ client/src/app/models/Team.ts
@@ -1,8 +1,10 @@
 import { Person } from './Person';
+import { Visit } from './Visit';
 
 export class Team{
     private id: number;
     private prefix: string;
+    private visits: Visit[];
     private members: Person[];
 
     constructor(id: number, members: Person[]) {
@@ -15,6 +17,21 @@ export class Team{
 
     getPrefix(){
         return this.prefix;
+    }
+
+    getVisits(){
+        return this.visits
+    }
+
+    addVisit(visit: Visit){
+        this.visits.push(visit);
+    }
+
+    removeVisit(visit: Visit){
+        const index = this.visits.indexOf(visit, 0)
+        if (index > -1){
+            this.visits.splice(index, 1)
+        }
     }
 
     getMembers(){
