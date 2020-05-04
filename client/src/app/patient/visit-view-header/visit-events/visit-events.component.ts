c573e963cc6b0a95d9e7ccfe472b278053257f82
simon <simon@simonws.se>
2020-05-04 11:36:57 +0200

    Add event list

--- client/src/app/patient/visit-view-header/visit-events/visit-events.component.ts
+++ client/src/app/patient/visit-view-header/visit-events/visit-events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {EventComponent} from '../../../event/event.component';
 
 @Component({
   selector: 'app-visit-events',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visit-events.component.css']
 })
 export class VisitEventsComponent implements OnInit {
+  events: EventComponent[];
 
   constructor() { }
 
