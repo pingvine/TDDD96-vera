884132d2a41d06eebe2ac664743c4e740fd3fe90
JoRu <johru036@student.liu.se>
2020-05-04 09:27:52 +0200

    Update css for material design.

--- client/src/app/patient-status-edit-view-e/patient-status-edit-view-e.component.ts
+++ client/src/app/patient-status-edit-view-e/patient-status-edit-view-e.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientStatusEditViewEComponent implements OnInit {
   panelOpenState = false;
+  sarskada = false;
+  brannskada = false;
+  hudutslag = false;
   constructor() { }
 
   ngOnInit(): void {
