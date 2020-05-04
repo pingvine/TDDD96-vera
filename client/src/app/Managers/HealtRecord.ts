be889666ab8f987a095c55cad7c784def7297120
Jesper Jonsson <jesjo430@student.liu.se>
2020-05-04 14:21:44 +0200

    Create init draft HealthRecord

---
+++ client/src/app/Managers/HealtRecord.ts
@@ -0,0 +1,11 @@
+function getData(ehrId: number, data: string){
+    return 'true'
+}
+
+function getEhrId(socialId: number){
+    return 1
+}
+
+function postData(id: number, data: string){
+    return 'true'
+}
