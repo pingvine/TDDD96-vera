b7e8160a5fc869e9526a8e16a698bc183209ec9e
simon <simon@simonws.se>
2020-05-04 15:16:14 +0200

    Add inputs for vital params

--- client/src/app/patient/visit-view-header/visit-body/visit-body.component.ts
+++ client/src/app/patient/visit-view-header/visit-body/visit-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-visit-body',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visit-body.component.css']
 })
 export class VisitBodyComponent implements OnInit {
+  @Input() spo2: number;  // Blood saturation
+  @Input() af: number;    // Airflow count per minute
+  @Input() pulse: number;
+  @Input() bt: number;    // Blood pressure
+  @Input() rls: string;   // Consciousness
+  @Input() temp: number;  // Body temperature
+  @Input() pain: number;  // Pain estimate // Todo validator
+  @Input() weight: number;
 
   constructor() { }
 
