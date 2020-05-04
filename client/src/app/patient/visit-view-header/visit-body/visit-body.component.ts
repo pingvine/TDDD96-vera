0e4826529959ecc7470c0a5f85d7929ec645a796
simon <simon@simonws.se>
2020-05-04 11:30:38 +0200

    Add visit body component

---
+++ client/src/app/patient/visit-view-header/visit-body/visit-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-body',
+  templateUrl: './visit-body.component.html',
+  styleUrls: ['./visit-body.component.css']
+})
+export class VisitBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
