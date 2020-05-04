b49af57ea60bb6d45551cccf296adf3c49d6eccf
simon <simon@simonws.se>
2020-05-04 11:31:55 +0200

    add visit events

---
+++ client/src/app/patient/visit-view-header/visit-events/visit-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-events',
+  templateUrl: './visit-events.component.html',
+  styleUrls: ['./visit-events.component.css']
+})
+export class VisitEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
