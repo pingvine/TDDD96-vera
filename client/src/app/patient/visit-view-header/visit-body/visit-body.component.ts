a5523ddf681498262772207a1cfe25338b6bf665
simon <simon@simonws.se>
2020-05-05 18:00:27 +0200

    bind data

--- client/src/app/patient/visit-view-header/visit-body/visit-body.component.ts
+++ client/src/app/patient/visit-view-header/visit-body/visit-body.component.ts
@@ -10,7 +10,7 @@ export class VisitBodyComponent implements OnInit {
   @Input() af: number;    // Airflow count per minute
   @Input() pulse: number;
   @Input() bt: number;    // Blood pressure
-  @Input() rls: string;   // Consciousness
+  @Input() conscious: string;   // Consciousness, GCS, RLS
   @Input() temp: number;  // Body temperature
   @Input() pain: number;  // Pain estimate // Todo validator
   @Input() weight: number;
