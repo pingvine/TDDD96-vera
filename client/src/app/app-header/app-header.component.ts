939e0b798c87eebbe88261494f7fb81675278265
Tommy Johansson <tomjo891@student.liu.se>
2020-05-06 16:00:59 +0200

    Add databinding between AppHeader and Notice

--- client/src/app/app-header/app-header.component.ts
+++ client/src/app/app-header/app-header.component.ts
@@ -7,11 +7,27 @@ import { ViewNameService } from '../view-name.service';
   styleUrls: ['./app-header.component.css']
 })
 export class AppHeaderComponent implements OnInit {
-  alerts = [].length;
   currentView: string;
+  notices = [{gender: 'male', type: 'important', name: 'Johan Berglund', personalId: '19580101-0102', age: 62, team: 'Team A', timeSent: '10.35', title: 'Titta till patient'}];
+  alerts = this.notices.length;
 
   constructor(private viewNameService: ViewNameService) {
     this.viewNameService.view$.subscribe(view => this.currentView = view);
+  }
+
+  addNotice(notice: any): void {
+    this.notices.push(notice);
+    this.updateAlerts();
+  }
+
+  removeNotice(notice: any): void {
+    const index = this.notices.indexOf(notice);
+    this.notices.splice(index, 1);
+    this.updateAlerts();
+  }
+
+  updateAlerts(): void {
+    this.alerts = this.notices.length;
   }
 
   ngOnInit(): void {
--- client/src/app/notice/notice.component.ts
+++ client/src/app/notice/notice.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
   styleUrls: ['./notice.component.css']
 })
-export class NoticeComponent implements OnInit {
+export class NoticeComponent implements OnInit, OnChanges {
+  @Input() notice: any;
+  @Output() deleteNoticeSelector = new EventEmitter<any>();
   gender: string;
   type: string;
   name: string;
-  socialId: string;
+  personalId: string;
   age: number;
   team: string;
   timeSent: string;
   title: string;
   constructor() {
-    this.gender = 'male';
-    this.type = 'info';
-    this.name = 'Johan Berglund';
-    this.socialId = '19580101-0102';
-    this.age = 62;
-    this.team = 'Team A';
-    this.timeSent = '10.35';
-    this.title = 'Ny Patient';
+  }
+
+  deleteNotice(): void {
+    this.deleteNoticeSelector.emit(this.notice);
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.gender = this.notice.gender;
+    this.type = this.notice.type;
+    this.name = this.notice.name;
+    this.personalId = this.notice.personalId;
+    this.age = this.notice.age;
+    this.team = this.notice.team;
+    this.timeSent = this.notice.timeSent;
+    this.title = this.notice.title;
   }
 
 }
