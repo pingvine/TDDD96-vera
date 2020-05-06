89c92a8afaab4616f0aae210262c75f100e14e42
simon <simon@simonws.se>
2020-05-06 12:44:52 +0200

    Add button for sendmsg and method in component

--- client/src/app/test-event-socket/test-event-socket.component.ts
+++ client/src/app/test-event-socket/test-event-socket.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {EventSocketService} from "../services/event-socket.service";
+import { EventSocketService } from '../services/event-socket.service';
 
 @Component({
   selector: 'app-test-event-socket',
   templateUrl: './test-event-socket.component.html',
-  styleUrls: ['./test-event-socket.component.css']
+  styleUrls: ['./test-event-socket.component.css'],
 })
 export class TestEventSocketComponent implements OnInit {
-
   constructor(private eventService: EventSocketService) { }
 
   ngOnInit(): void {
+    this.eventService.connect();
   }
 
+  sendMessage() {
+    this.eventService.sendMessage('TestMessageFromClient');
+  }
 }
