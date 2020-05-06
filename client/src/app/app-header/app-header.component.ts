5a76eff0b06e8ae7e159928c7627e352dafb95ec
Tommy Johansson <tomjo891@student.liu.se>
2020-05-06 21:08:50 +0200

    Add preemptive implementation for creation and broadcasting of notices

--- client/src/app/app-header/app-header.component.ts
+++ client/src/app/app-header/app-header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewNameService } from '../view-name.service';
+import {WebSocketSubject} from "rxjs/internal-compatibility";
+import {Message} from "../models/Message";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -10,9 +13,17 @@ export class AppHeaderComponent implements OnInit {
   currentView: string;
   notices = [{gender: 'male', type: 'important', name: 'Johan Berglund', personalId: '19580101-0102', age: 62, team: 'Team A', timeSent: '10.35', title: 'Titta till patient'}];
   alerts = this.notices.length;
+  private socket$: WebSocketSubject<Message>;
 
   constructor(private viewNameService: ViewNameService) {
     this.viewNameService.view$.subscribe(view => this.currentView = view);
+    this.socket$ = new WebSocketSubject('ws://localhost:80');
+
+    this.socket$.subscribe(
+      notice => this.addNotice(notice),
+      error => console.error(error),
+      () => console.warn('Notice received')
+    );
   }
 
   addNotice(notice: any): void {
@@ -28,6 +39,19 @@ export class AppHeaderComponent implements OnInit {
 
   updateAlerts(): void {
     this.alerts = this.notices.length;
+  }
+
+  createNotice(gender: string, type: string, name: string,
+               personalId: string, age: number, team: string,
+               currentTime: string, title: string, sender: string, receivers: string[]): any {
+    const notice = {gender: gender, type: type, name:  name, personalId: personalId, age: age, team: team, timeSent: currentTime, title: title};
+    this.sendNotice(sender, notice, receivers);
+  }
+
+  sendNotice(senderTeam: string, notice: any, receivers: string[]): void {
+    const message = new Message(senderTeam, notice, receivers);
+
+    this.socket$.next(message);
   }
 
   ngOnInit(): void {
