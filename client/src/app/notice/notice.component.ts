b1f8bc177d924eb1c3d4438ea95bdca694c80077
Tommy Johansson <tomjo891@student.liu.se>
2020-05-06 12:10:07 +0200

    Adjust design of neticecards slightly

--- client/src/app/notice/notice.component.ts
+++ client/src/app/notice/notice.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class NoticeComponent implements OnInit {
   gender: string;
   type: string;
+  name: string;
+  socialId: string;
+  age: number;
+  team: string;
+  timeSent: string;
+  title: string;
   constructor() {
     this.gender = 'male';
     this.type = 'info';
+    this.name = 'Johan Berglund';
+    this.socialId = '19580101-0102';
+    this.age = 62;
+    this.team = 'Team A';
+    this.timeSent = '10.35';
+    this.title = 'Ny Patient';
   }
 
   ngOnInit(): void {
