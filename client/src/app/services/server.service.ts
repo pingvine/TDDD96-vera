ac792a9fdec667ab45cb948ea1ff343e5133258c
simon <simon@simonws.se>
2020-05-07 12:05:40 +0200

    add url, header, and httpclient

--- client/src/app/services/server.service.ts
+++ client/src/app/services/server.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const baseUrl = 'http://localhost:4201';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
 }
