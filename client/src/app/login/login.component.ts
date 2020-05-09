d9b11b331096cd10f749df93614c0fca2e2163e7
simon <simon@simonws.se>
2020-05-09 13:38:48 +0200

    Add logo, matforms

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RoleType} from "../models/RoleType";
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  roles: RoleType[] = [RoleType.Administrator, RoleType.AssistingNurse]
 
   constructor() { }
 
