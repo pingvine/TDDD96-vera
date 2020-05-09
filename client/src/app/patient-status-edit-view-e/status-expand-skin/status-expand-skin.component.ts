f6ee6575031b0b3190bddb90182961449ebaf134
Johan Runestam <johru036@student.liu.se>
2020-05-09 13:21:06 +0200

    Add expand skin component.

---
+++ client/src/app/patient-status-edit-view-e/status-expand-skin/status-expand-skin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-expand-skin',
+  templateUrl: './status-expand-skin.component.html',
+  styleUrls: ['./status-expand-skin.component.css']
+})
+export class StatusExpandSkinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
