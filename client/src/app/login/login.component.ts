0fa71cb1b968212d819ae5c6ab19adbd78e890b5
simon <simon@simonws.se>
2020-05-09 13:52:23 +0200

    Add login selection

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,17 +1,27 @@
-import {Component, OnInit} from '@angular/core';
-import {RoleType} from "../models/RoleType";
+import { Component, OnInit } from '@angular/core';
+import { RoleType } from '../models/RoleType';
+
+interface Role {
+  value: RoleType;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  roles: RoleType[] = [RoleType.Administrator, RoleType.AssistingNurse]
+  roles: Role[] = [
+    { value: RoleType.AssistingNurse, viewValue: 'USK' },
+    { value: RoleType.Nurse, viewValue: 'SSK' },
+    { value: RoleType.Doctor, viewValue: 'LÄK' },
+    { value: RoleType.Administrator, viewValue: 'Admin' },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
