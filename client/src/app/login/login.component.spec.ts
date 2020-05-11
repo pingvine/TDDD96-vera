dfca72e203f3611a1bcb1b4eaedce28d162aef49
simon <simon@simonws.se>
2020-05-11 16:57:01 +0200

    Add declarations to login test

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ServerService} from "../services/server.service";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +11,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule, MatDialogModule],
+      providers: [ ServerService, MatDialog ]
     })
     .compileComponents();
   }));
