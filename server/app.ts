6f1fb7d3dc1c06552d58126da701390e8d187d98
Jesper Jonsson <jesjo430@student.liu.se>
2020-05-12 10:54:49 +0200

    Refactor code placement dbHelper.

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const eventserver = require('./websockets/server');
 const testRouter = require('./routes/test');
 const indexRouter = require('./routes/index');
@@ -16,18 +15,6 @@ const patientRouter = require('./routes/patient');
 const summaryRouter = require('./routes/summary');
 const bodyParser = require('body-parser');
 
-
-// TODO replace with db
-const users = [];
-let idCounter = 0;
-
-mongoose.connect('mongodb://localhost/coronavirus', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB!');
-});
 
 // Only accept trusted connections
 const corsOptions = {
@@ -102,19 +89,6 @@ app.post('/id', (req, res) => {
   idCounter += 1;
 });
 
-
-function userExists(socialId, userss) {
-  // Doesn't work atm
-  console.log(`socialid ${socialId}`);
-  userss.forEach((user) => {
-    console.log(`SocialId comp: ${user.socialId}`);
-    if (socialId === user.socialId) {
-      return true;
-    }
-  });
-  return false;
-}
-
 app.post('/user', (req, res) => {
   console.log('Request body:');
   console.log(req.body);
@@ -127,42 +101,4 @@ app.post('/user', (req, res) => {
   }
 });
 
-
-
-
 eventserver.runWebSocketServer();
-
-
-function initDb() {
-  /*
-  TESTKOD FÖR DATABAS
-   */
-  // console.log("JSON VERA: " + JSON.stringify(new EventVera("test")))
-
-  const data = {
-    fieldId: '1',
-    status: true,
-  };
-
-  const editEvent = new EventVera('simon', EventType.EditEvent, data);
-
-  const eventSchema = new mongoose.Schema({
-    senderId: String,
-    eventType: Number,
-    data: {},
-  });
-  const EventModel = mongoose.model('eventvera', eventSchema);
-  const event = new EventModel(editEvent);
-  event.save((err, val) => {
-    console.log(`save: ${val}`);
-  }); // Save to mongodb db
-
-  EventModel.findOne({ senderId: 'simon' }, (err, eventOne) => {
-    console.log(`docs ${eventOne}`);
-  });
-
-  EventModel.deleteMany({ senderId: 'simon' }, (err, val) => {
-    console.log(`Err: ${err}val ${val}`);
-  });
-}
-module.exports = app;
