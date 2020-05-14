caa830d713c7694216ccf4547b592bfdc5893a1a
Josef Atoui <josat799@student.liu.se>
2020-05-14 15:08:00 +0200

    Add functionality to add reasons for visit

--- client/src/app/new-visit/new-visit-reason/new-visit-reason.component.ts
+++ client/src/app/new-visit/new-visit-reason/new-visit-reason.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-visit-reason.component.css']
 })
 export class NewVisitReasonComponent implements OnInit {
+  reason: string;
+  ess: string;
+  information: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  updateReason(reason: any): void {
+    console.log(reason.target.value);
+    this.reason = reason.target.value;
+  }
+  updateESS(ess: any): void {
+    console.log(ess.target.value);
+    this.ess = ess.target.value;
+  }
+  updateInformation(information: any): void {
+    console.log(information.target.value);
+    this.information = information.target.value;
+  }
 }
